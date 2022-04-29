c2a7b1e81a5409f1e030a745482afffc2cc59702
Su Kang <sukang2017@u.northwestern.edu>
2022-04-29 15:00:41 -0500

    Added @RequestMapping example

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,6 +2,8 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -20,5 +22,14 @@ public class HelloController {
     @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring!";
+    }
+
+//    if you want a controller method that handles
+//    more than one method, say GET and POST you
+//    can use @RequestMapping
+    @RequestMapping(value = "hellogoodbye",method={RequestMethod.GET,RequestMethod.POST})
+    @ResponseBody
+    public String helloGoodbye(){
+        return "hello goodbye";
     }
 }
