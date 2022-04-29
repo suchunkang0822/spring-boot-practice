cd288c010e51c6f8fbe489f59b374564075c5274
Su Kang <sukang2017@u.northwestern.edu>
2022-04-29 14:30:56 -0500

    Implemented simple controller with static responses

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring-boot-practice
+starter- Basic springboot setup 
+
+controller annotations-Setting up a simple controller with required annotattions and methods to create request handlers
---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,24 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+//    handles request at the root
+//    @GetMapping
+//    handles request at path /hello
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello, Spring!";
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye, Spring!";
+    }
+}
